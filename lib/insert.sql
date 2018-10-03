6dba86476c016d8001a19b4bd1e76a1c9ce4ca02
github username <github email address>
2018-10-02 22:20:25 -0400

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 30, "M", "brown", "happy", 1);
- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 20, "M", "brown", "angry", 0);
- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 25, "F", "black", "melancholy", 1);
- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 35, "F", "grizzly", "sad", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES  (5, "Melissa", 15, "F", "polar", "stoic", 1);
- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 40, "M", "black", "grouchy", 0);
- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 45, "F", "brown", "lonely", 1);
- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 50, "M", "polar", "happy", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 30, "M", "brown", "happy", 1),
+ (2, "Rowdy", 20, "M", "brown", "angry", 0),
+ (3, "Tabitha", 25, "F", "black", "melancholy", 1),
+ (4, "Sergeant Brown", 35, "F", "grizzly", "sad", 0),
+ (5, "Melissa", 15, "F", "polar", "stoic", 1),
+ (6, "Grinch", 40, "M", "black", "grouchy", 0),
+ (7, "Wendy", 45, "F", "brown", "lonely", 1),
+ (8, null, 50, "M", "polar", "happy", 0);
 
 
 -- Mr. Chocolate
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,6 +1,7 @@
  def selects_all_female_bears_return_name_and_age
-   "SELECT name, age FROM bears where gender = 'F';"
+   "SELECT name, age FROM bears WHERE gender = 'F';"
  end
+
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "SELECT name FROM bears ORDER BY name;"
@@ -15,20 +16,16 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER by age ASC LIMIT 1;"
+  "SELECT name, age FROM bears ORDER BY age LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COLOR, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1;"
-
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1;"
 end
 
-# def selects_most_prominent_color_and_returns_with_count
-#   "SELECT bears.color, COUNT(bears.color) FROM bears GROUP BY bears.color ORDER BY COUNT(*) DESC LIMIT 1;"
-# end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament) FROM bears where temperament = 'goofy';"
+  "SELECT COUNT(*) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
